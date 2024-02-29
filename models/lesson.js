d47bcfcfe727db71ca261af023fc5006076b25e2
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-03-01 01:49:45 +0400

    LessonModelAndRouterAdded

---
+++ models/lesson.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const LessonSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    isFree: {
+        type: Boolean,
+        default: false
+    },
+    cover: {
+        type: String,
+        required: true
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now()
+    },
+    // comments: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Comments'
+    // },
+    // likes: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Likes'
+    // }
+});
+
+const Lesson = mongoose.model('Lesson', LessonSchema);
+
+exports.Lesson = Lesson;
---
+++ routes/lesson.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {});
+router.get('/:lid', async (req, res) => {});
+router.post('/', async (req, res) => {});
+router.put('/:lid', async (req, res) => {});
+router.delete('/:lid', async (req, res) => {});
+
+module.exports = router;
