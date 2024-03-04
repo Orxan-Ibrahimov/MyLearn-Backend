73d988b7829ff7db0616b4eae329f24cc582800b
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-03-04 12:36:53 +0400

    CommentRouterAdded

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const citizensRouter = require('./routes/citizenship');
 const ratingDetailsRouter = require('./routes/rating-details');
 const lessonRouter = require('./routes/lesson');
 const userRouter = require('./routes/user');
+const commentRouter = require('./routes/comment');
 
 app.use(morgan('tiny'))
 app.use(bodyParser.json());
@@ -21,6 +22,7 @@ app.use(`${API_URL}/citizenships`, citizensRouter);
 app.use(`${API_URL}/ratingDetails`, ratingDetailsRouter);
 app.use(`${API_URL}/lessons`, lessonRouter);
 app.use(`${API_URL}/users`, userRouter);
+app.use(`${API_URL}/comments`, commentRouter);
 
 mongoose
   .connect(process.env.DATABASE_CONNECTION, {
---
+++ routes/comment.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {});
+router.get("/:cid", async (req, res) => {});
+router.post("/", async (req, res) => {});
+router.put("/:cid", async (req, res) => {});
+router.delete("/:cid", async (req, res) => {});
+
+module.exports = router;
