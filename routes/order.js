04e13819105dda06f0abe841d0a86ae66039bc5b
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-03-06 02:15:04 +0400

    OrderPostRequestsAdded

--- routes/order.js
+++ routes/order.js
@@ -4,9 +4,25 @@ const { User } = require("../models/user");
 const { Playlist } = require("../models/playlist");
 const router = express.Router();
 
-router.get("/", async (req, res) => {});
-router.get("/:eid", async (req, res) => {});
+// Orders GET Request To Get The Orders List
+router.get("/", async (req, res) => {
+  const ordersList = await Order.find().populate(["subscriber", "course"]);
 
+  if(!ordersList) return res.status(400).json({success: false, message: "Not found any order!"});
+
+  res.status(200).send(ordersList);
+});
+
+// Orders GET Request To Get The Order By Id
+router.get("/:oid", async (req, res) => {
+  const order = await Order.findById(req.params.oid).populate(["subscriber", "course"]);
+
+  if(!order) return res.status(400).json({success: false, message: "The order not found!"});
+
+  res.status(200).send(order);
+});
+
+// Orders POST Request To Add The Order To The Orders List
 router.post("/", async (req, res) => {
   let order = new Order({
     subscriber: req.body.subscriber,
@@ -43,6 +59,7 @@ router.post("/", async (req, res) => {
   course = await course.save();
   res.status(201).send(order);
 });
+
 router.delete("/:eid", async (req, res) => {});
 
 module.exports = router;
