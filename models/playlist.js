49a04b3b0595f539bbbe3f8743f163cc395c5bfe
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-03-05 14:26:10 +0400

    PlaylistModelAdded

---
+++ models/playlist.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const playlistSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  cover: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  subscribers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  // ratings: [
+  //     {
+  //         type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'Rating',
+  //     }
+  // ]
+});
+
+const Playlist = mongoose.Schema("Playlist", playlistSchema);
+
+exports.Playlist = Playlist;
