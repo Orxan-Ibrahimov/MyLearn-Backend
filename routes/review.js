c489472951b7e18ab939d66e1f9c7dcea56a9304
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-03-08 15:59:48 +0400

    ReviewDeleteRequestAdded

--- routes/review.js
+++ routes/review.js
@@ -17,14 +17,14 @@ router.get("/", async (req, res) => {
 
 // Reviews GET Request To Get The Review By Id
 router.get("/:rid", async (req, res) => {
-    const review = await Review.findById(req.params.rid);
+  const review = await Review.findById(req.params.rid);
 
-    if (!review)
-      return res
-        .status(404)
-        .json({ success: false, message: "The review not found!" });
-  
-    res.status(200).send(review);
+  if (!review)
+    return res
+      .status(404)
+      .json({ success: false, message: "The review not found!" });
+
+  res.status(200).send(review);
 });
 
 // Reviews POST Request To Add A New Review To The Reviews List
@@ -56,8 +56,28 @@ router.post("/", async (req, res) => {
   res.status(201).send(review);
 });
 
-
 router.put("/:rid", async (req, res) => {});
-router.delete("/:rid", async (req, res) => {});
+router.delete("/:rid", async (req, res) => {
+  const review = await Review.findByIdAndDelete(req.params.rid)
+    .then(async (removedReview) => {
+      if (!removedReview)
+        return res
+          .status(500)
+          .json({ success: false, message: "The review can not be deleted!" });
+
+      user = await User.findById(removedReview.user);
+      if (user) {
+        if (user.reviews.includes(removedReview.id)) {
+          user.reviews.splice(removedReview.id);
+          user = await user.save();
+        }
+      }
+      res.status(200).send(removedReview);
+    })
+    .catch((err) => {
+      if (err)
+        return res.status(500).json({ success: false, message: err.message });
+    });
+});
 
 module.exports = router;
